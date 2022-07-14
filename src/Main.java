60eb8543078817b0a8522f5475313b98c434dcdd
Peter Freitas <opeterfreitas@gmail.com>
2022-07-14 13:48:17 -0300

    Mentoria e Curso

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import com.opeterfreitas.desafio.dominio.Curso;
+import com.opeterfreitas.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descrição curso Java");
+        curso1.setCargaHoraria(18);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso Spring Framework");
+        curso2.setDescricao("Descrição curso Spring Framework");
+        curso2.setCargaHoraria(12);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria de Java");
+        mentoria.setDescricao("Descrição mentoria de Java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+
+    }
+}
