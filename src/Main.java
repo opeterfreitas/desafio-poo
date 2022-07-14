b99f2d0cd201191246e11653649182b0b6a0952a
Peter Freitas <opeterfreitas@gmail.com>
2022-07-14 14:43:26 -0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-import com.opeterfreitas.desafio.dominio.Conteudo;
-import com.opeterfreitas.desafio.dominio.Curso;
-import com.opeterfreitas.desafio.dominio.Mentoria;
+import com.opeterfreitas.desafio.dominio.*;
 
 import java.time.LocalDate;
 
@@ -22,9 +20,34 @@ public class Main {
         mentoria.setDescricao("Descrição mentoria de Java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);
+//        System.out.println(curso1);
+//        System.out.println(curso2);
+//        System.out.println(mentoria);
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev dev1 = new Dev();
+        dev1.setNome("Peter");
+        dev1.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos: " + dev1.getConteudosInscritos());
+        dev1.progredir();
+        dev1.progredir();
+        System.out.println("Conteúdos Concluídos: " + dev1.getConteudosConcluidos());
+        System.out.println("XP: " + dev1.calcularTotalXp());
+
+        Dev dev2 = new Dev();
+        dev2.setNome("Ciclano");
+        dev2.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos: " + dev2.getConteudosInscritos());
+        dev2.progredir();
+        System.out.println("Conteúdos Concluídos: " + dev2.getConteudosConcluidos());
+        System.out.println("XP: " + dev2.calcularTotalXp());
+
 
     }
 }
